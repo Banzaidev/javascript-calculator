776267c1bcda3d364b29114e2ad38cab44efe5b0
Banzaidev <66788936+Banzaidev@users.noreply.github.com>
2023-12-23 20:15:52 +0100

    style added

--- main.js
+++ main.js
@@ -9,17 +9,21 @@ import { addNumber, valueStore, basicOperation, submit,clear} from './store';
 
 $('#app').html(
   `
+    <h1 id='title'>Javascript Calculator</h1> 
     <div id='calculator'>
       <div id='display'>${valueStore.getState().value}</div>
       <div id='prevValue'></div>
-      <div id='numbers'>
-        ${numbers.map((number=> NumberButtons(number.id, number.value))).join('')}
-      </div>
-      <div id='operations'>
-        ${operations.map(operation => OperationButtons(operation.id, operation.value)).join('')}
-      </div>
+      <div id='container'>
+        <div id='numbers'>
+          ${numbers.map((number=> NumberButtons(number.id, number.value))).join('')}
+        </div>
+        <div id='operations'>
+          ${operations.map(operation => OperationButtons(operation.id, operation.value)).join('')}
+        </div>
       
+      </div>
     </div>
+    <p id="author">by <a target='_blank' href='https://github.com/Banzaidev'>banzaidev</a></p>
   `
 )
 $.when($.ready).then(()=>{
